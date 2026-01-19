af3853c923d8e97edf1923cd958eb76c07a49292
Sipun693 <122521613+Sipun693@users.noreply.github.com>
2026-01-19 16:04:17 +0530

    commit

--- factorial.java
+++ factorial.java
@@ -8,15 +8,15 @@ public class factorial{
         Scanner st=new Scanner(System.in);
         System.out.println("enter your number");
         int num= st.nextInt();
-        int z=num;
+        int z=1;
         if(num==0||num==1)
         {
             System.out.println("Factorial of "+num+" is 1");
 
         }
-        for(int i=1;i<=num-1 && num!=0; i++)
+        for(int i=1;i<=num; i++)
         {
-            z=z*num;
+            z=z*i;
 
         }
 
